10bf57986ad73e24b21765a9c55836cb05f92b01
박겸영 <yong_drg@naver.com>
2023-02-20 05:56:52 +0900

    feat(ResultPage): 페이지 레이아웃 통일 및 Route 추가

---
+++ src/pages/ResultPage/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ResultPageOutputs from '../../components/ResultPageOutputs';
+
+const ResultPage = () => {
+  return <ResultPageOutputs />;
+};
+
+export default ResultPage;
