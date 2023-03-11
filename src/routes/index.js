7c5f2dd5dc25ae8ed826630a3ec94a27b2d336f5
박겸영 <yong_drg@naver.com>
2023-03-11 14:30:01 +0900

    refactor: constants 폴더 생성

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from '../pages';
-import { PATH } from './config';
+import ROUTE_PATH from '../constants/routePath';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={PATH.default} element={<Main />} />
-        <Route path={PATH.result} element={<Main />} />
+        <Route path={ROUTE_PATH.default} element={<Main />} />
+        <Route path={ROUTE_PATH.result} element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
